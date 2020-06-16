2c0e5418d29b5b1a51a6413e1146de67594b6407
Glitch (2-8-dtech) <none>
2020-06-16 00:29:52 +0000

    V1

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function myFunction() {
+ var x = document.getElementById("demo");
+ if (x.className.indexOf("w3-show") == -1) {
+   x.className += " w3-show";
+ } else {
+   x.className = x.className.replace(" w3-show", "");
+ }
+}
